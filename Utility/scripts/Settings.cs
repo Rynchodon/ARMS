4e2607f4f35ff9ab1b5d056085298af52d18b7c2
Rynchodon <al.durand@gmail.com>
2015-05-09 20:29:29 -0400

    version 31

--- Utility/scripts/Settings.cs
+++ Utility/scripts/Settings.cs
@@ -31,7 +31,7 @@ namespace Rynchodon
 		private static System.IO.TextWriter settingsWriter;
 
 		private static string strVersion = "Version";
-		private static int latestVersion = 30; // in sequence of updates on steam
+		private static int latestVersion = 31; // in sequence of updates on steam
 
 		private static Logger myLogger = new Logger(null, "Settings");
 		[System.Diagnostics.Conditional("LOG_ENABLED")]
