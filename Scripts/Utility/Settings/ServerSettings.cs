21c2df43f67ad89181dd611113d4be3c6b9791f4
Rynchodon <al.durand@gmail.com>
2016-06-12 17:51:25 -0400

    version 71

--- Scripts/Utility/Settings/ServerSettings.cs
+++ Scripts/Utility/Settings/ServerSettings.cs
@@ -52,7 +52,7 @@ namespace Rynchodon.Settings
 		private const string strVersion = "Version";
 		private static System.IO.TextWriter settingsWriter;
 
-		public const int latestVersion = 70; // in sequence of updates on steam
+		public const int latestVersion = 71; // in sequence of updates on steam
 
 		public static readonly int fileVersion;
 
