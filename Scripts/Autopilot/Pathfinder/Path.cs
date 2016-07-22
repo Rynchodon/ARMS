0f461564da4ba71d162d9a920f98dd8b9565d99c
Rynchodon <al.durand@gmail.com>
2016-07-21 21:54:45 -0400

    sphere clusters

--- Scripts/Autopilot/Pathfinder/Path.cs
+++ Scripts/Autopilot/Pathfinder/Path.cs
@@ -1,87 +1,133 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Sandbox.Game.Entities;
 using VRage.Game.Entity;
 using VRageMath;
 
 namespace Rynchodon.Autopilot.Pathfinder
 {
-	public struct Path
+	public class PathThing
 	{
-		public Vector3D CurrentPosition, Destination;
-		public Vector3 AutopilotVelocity;
-		public double Distance, Radius;
-
-		public Path(ref Vector3D CurrentPosition, ref Vector3D Destination, ref Vector3 AutopilotVelocity, float Radius)
+		private struct Path
 		{
-			this.CurrentPosition = CurrentPosition;
-			this.Destination = Destination;
-			this.AutopilotVelocity = AutopilotVelocity;
-			this.Radius = Radius;
+			public Vector3D CurrentPosition, Destination;
+			public Vector3 AutopilotVelocity;
+			public double DistanceCurrentToDestination;
+			public float AutopilotShipBoundingRadius;
 
-			Vector3D.Distance(ref this.CurrentPosition, ref this.Destination, out this.Distance);
+			public Path(ref Vector3D Destination, MyCubeGrid autopilotGrid)
+			{
+				this.CurrentPosition = autopilotGrid.GetCentre();
+				this.Destination = Destination;
+				this.AutopilotVelocity = autopilotGrid.Physics.LinearVelocity;
+				this.AutopilotShipBoundingRadius = autopilotGrid.PositionComp.LocalVolume.Radius;
+
+				Vector3D.Distance(ref this.CurrentPosition, ref this.Destination, out this.DistanceCurrentToDestination);
+			}
 		}
 
-		// If the sum of all repulsions ends up being the opposite direction of Destination - CurrentPosition, we are stuck and need to waypoint around.
-		// it would be better if we could avoid creating a valley between repulsor obstructions in the first place.
-		// Atmospheric ship might want to consider space as repulsor???
-		public void Repulsion(MyEntity entity, out Vector3 repulse)
+		static PathThing()
 		{
-			Logger.DebugLog("entity is null", Logger.severity.FATAL, condition: entity == null);
-			Logger.DebugLog("entity is not top-most", Logger.severity.FATAL, condition: entity.Hierarchy.Parent != null);
+			Logger.SetFileName("PathThing");
+		}
 
-			Vector3D centre = entity.GetCentre();
-			float boundingRadius;
-			float linearSpeed;
+		private Path m_path;
+		private SphereClusters m_clusters = new SphereClusters();
 
-			MyPlanet planet = entity as MyPlanet;
-			if (planet != null)
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="allEntities">All the entities that may need to be avoided or generate repulsion.</param>
+		/// <param name="avoidEntities">Output list. Entities that are too close for repulsion and should be avoided.</param>
+		private void CalcRepulsion(List<MyEntity> allEntities, List<MyEntity> avoidEntities, out Vector3 repulsion)
+		{
+			if (m_clusters.Clusters.Count != 0)
+				m_clusters.Clear();
+
+			for (int index = allEntities.Count - 1; index >= 0; index--)
 			{
-				boundingRadius = planet.MaximumRadius;
-				linearSpeed = AutopilotVelocity.Length();
-			}
-			else
-			{
-				boundingRadius = entity.PositionComp.LocalVolume.Radius;
-				if (entity.Physics == null)
-					linearSpeed = AutopilotVelocity.Length();
+				MyEntity entity = allEntities[index];
+
+				Logger.DebugLog("entity is null", Logger.severity.FATAL, condition: entity == null);
+				Logger.DebugLog("entity is not top-most", Logger.severity.FATAL, condition: entity.Hierarchy.Parent != null);
+
+				Vector3D centre = entity.GetCentre();
+				float boundingRadius;
+				float linearSpeed;
+
+				MyPlanet planet = entity as MyPlanet;
+				if (planet != null)
+				{
+					boundingRadius = planet.MaximumRadius;
+					linearSpeed = m_path.AutopilotVelocity.Length();
+				}
 				else
-					linearSpeed = (entity.Physics.LinearVelocity - AutopilotVelocity).LengthSquared();
-			}
-			boundingRadius += linearSpeed * 10f; // Adjustable
+				{
+					boundingRadius = entity.PositionComp.LocalVolume.Radius;
+					if (entity.Physics == null)
+						linearSpeed = m_path.AutopilotVelocity.Length();
+					else
+						linearSpeed = (entity.Physics.LinearVelocity - m_path.AutopilotVelocity).Length();
+				}
+				boundingRadius += m_path.AutopilotShipBoundingRadius + linearSpeed * 10f; // Adjustable
 
-			Vector3 toCurrent = CurrentPosition - centre;
+				Vector3D toCurrent;
+				Vector3D.Subtract(ref m_path.CurrentPosition, ref centre, out toCurrent);
+				if (toCurrent.LengthSquared() < boundingRadius * boundingRadius)
+				{
+					// Entity is too close for repulsion.
+					avoidEntities.Add(entity);
+					continue;
+				}
+
+				BoundingSphereD entitySphere = new BoundingSphereD(centre, boundingRadius);
+				m_clusters.Add(ref entitySphere);
+			}
+
+			m_clusters.AddMiddleSpheres();
+
+			repulsion = Vector3D.Zero;
+			for (int indexO = m_clusters.Clusters.Count - 1; indexO >= 0; indexO--)
+			{
+				List<BoundingSphereD> cluster = m_clusters.Clusters[indexO];
+				for (int indexI = cluster.Count - 1; indexI >= 0; indexI--)
+				{
+					BoundingSphereD sphere = cluster[indexI];
+					CalcRepulsion(ref sphere, ref repulsion);
+				}
+			}
+		}
+
+		// TODO: Atmospheric ship might want to consider space as repulsor???
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="sphere">The sphere which is repulsing the autopilot.</param>
+		/// <param name="repulsion">A directional vector with length between 0 and 1, indicating the repulsive force.</param>
+		public void CalcRepulsion(ref BoundingSphereD sphere, ref Vector3 repulsion)
+		{
+			Vector3D toCurrentD;
+			Vector3D.Subtract(ref m_path.CurrentPosition, ref sphere.Center, out toCurrentD);
+			Vector3 toCurrent = toCurrentD;
 			float toCurrentLenSq = toCurrent.LengthSquared();
 
-			if (toCurrentLenSq < boundingRadius * boundingRadius)
-			{
-				// Autopilot is too close to entity for repulsion, need to avoid it.
-				Logger.DebugLog("autopilot is too close to entity for repulsion. entity: " + entity.nameWithId() + ", toCurrentLenSq:" + toCurrentLenSq, Logger.severity.FATAL);
-				throw new NotImplementedException();
-			}
-
-			float maxRepulseDist = boundingRadius + boundingRadius; // Adjustable, might need to be different for planets than other entities.
+			float maxRepulseDist = (float)(sphere.Radius + sphere.Radius); // Adjustable, might need to be different for planets than other entities.
 			float maxRepulseDistSq = maxRepulseDist * maxRepulseDist;
 
 			if (toCurrentLenSq > maxRepulseDistSq)
 			{
 				// Autopilot is outside the maximum bounds of repulsion.
-				repulse = Vector3.Zero;
 				return;
 			}
 
 			// If both entity and autopilot are near the destination, the repulsion radius must be reduced or we would never reach the destination.
 			double toDestLenSq;
-			Vector3D.DistanceSquared(ref centre, ref Destination, out toDestLenSq);
-			toDestLenSq += Distance * Distance; // Adjustable
+			Vector3D.DistanceSquared(ref sphere.Center, ref  m_path.Destination, out toDestLenSq);
+			toDestLenSq += m_path.DistanceCurrentToDestination * m_path.DistanceCurrentToDestination; // Adjustable
 
 			if (toCurrentLenSq > toDestLenSq)
 			{
 				// Autopilot is outside the boundaries of repulsion.
-				repulse = Vector3.Zero;
 				return;
 			}
 
@@ -89,7 +135,11 @@ namespace Rynchodon.Autopilot.Pathfinder
 				maxRepulseDist = (float)Math.Sqrt(toDestLenSq);
 
 			// maxRepulseDist / toCurrentLenSq can be adjusted
-			Vector3.Multiply(ref toCurrent, maxRepulseDist / toCurrentLenSq, out repulse);
+			Vector3 result;
+			Vector3.Multiply(ref toCurrent, maxRepulseDist / toCurrentLenSq - 1f, out result);
+			Vector3 result2;
+			Vector3.Add(ref repulsion, ref result, out result2);
+			repulsion = result2;
 		}
 	}
 }
---
+++ Scripts/Autopilot/Pathfinder/SphereClusters.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections.Generic;
+using Sandbox.ModAPI;
+using VRage.Collections;
+using VRageMath;
+
+namespace Rynchodon.Autopilot.Pathfinder
+{
+	public class SphereClusters
+	{
+		private static MyConcurrentPool<List<BoundingSphereD>> m_sphereLists = new MyConcurrentPool<List<BoundingSphereD>>();
+
+		static SphereClusters()
+		{
+			Logger.SetFileName("SphereClusters");
+			MyAPIGateway.Entities.OnCloseAll += Entities_OnCloseAll;
+		}
+
+		private static void Entities_OnCloseAll()
+		{
+			MyAPIGateway.Entities.OnCloseAll -= Entities_OnCloseAll;
+			m_sphereLists = null;
+		}
+
+		public readonly List<List<BoundingSphereD>> Clusters = new List<List<BoundingSphereD>>();
+
+		public void Add(ref BoundingSphereD sphere)
+		{
+			List<BoundingSphereD> joinedCluster = null;
+			List<BoundingSphereD> emptyCluster = null;
+
+			for (int indexO = Clusters.Count - 1; indexO >= 0; indexO--)
+			{
+				List<BoundingSphereD> cluster = Clusters[indexO];
+				if (cluster.Count == 0)
+				{
+					emptyCluster = cluster;
+					continue;
+				}
+				for (int indexI = cluster.Count - 1; indexI >= 0; indexI--)
+				{
+					BoundingSphereD checkSphere = cluster[indexI];
+
+					double distSq;
+					Vector3D.DistanceSquared(ref sphere.Center, ref checkSphere.Center, out distSq);
+
+					double radii = sphere.Radius + checkSphere.Radius;
+					radii *= radii;
+
+					if (distSq <= radii)
+					{
+						if (joinedCluster != null)
+						{
+							joinedCluster.AddList(cluster);
+							cluster.Clear();
+						}
+						else
+						{
+							joinedCluster = cluster;
+							cluster.Add(sphere);
+						}
+						break;
+					}
+				}
+			}
+
+			if (joinedCluster == null)
+			{
+				joinedCluster = emptyCluster != null ? emptyCluster : m_sphereLists.Get();
+				joinedCluster.Add(sphere);
+				Clusters.Add(joinedCluster);
+			}
+		}
+
+		/// <summary>
+		/// Add the spheres that prevent autopilot from getting stuck between objects.
+		/// </summary>
+		/// <remarks>
+		/// For every cluster, adds a sphere that includes the centre of every sphere in the cluster.
+		/// </remarks>
+		public void AddMiddleSpheres()
+		{
+			for (int indexO = Clusters.Count - 1; indexO >= 0; indexO--)
+			{
+				List<BoundingSphereD> cluster = Clusters[indexO];
+				if (cluster.Count < 2)
+					continue;
+				Vector3D[] points = new Vector3D[cluster.Count];
+				for (int indexI = cluster.Count - 1; indexI >= 0; indexI--)
+					points[indexI] = cluster[indexI].Center;
+				cluster.Add(BoundingSphereD.CreateFromPoints(points));
+			}
+		}
+
+		public void Clear()
+		{
+			for (int index = Clusters.Count - 1; index >= 0; index--)
+			{
+				List<BoundingSphereD> cluster = Clusters[index];
+				cluster.Clear();
+				m_sphereLists.Return(cluster);
+			}
+			Clusters.Clear();
+		}
+
+	}
+}
