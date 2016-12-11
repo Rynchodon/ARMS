7c95511791f7d3e9236a94d00f56070c03d210b3
Rynchodon <al.durand@gmail.com>
2016-12-10 19:39:07 -0500

    draw air speed & drag

--- Scripts/Autopilot/Movement/AeroEffects.cs
+++ Scripts/Autopilot/Movement/AeroEffects.cs
@@ -5,13 +5,20 @@
 #endif
 
 using System;
+using System.Reflection;
 using System.Text;
-using Rynchodon.Update;
 using Sandbox.Common.ObjectBuilders;
 using Sandbox.Game.Entities;
-using Sandbox.ModAPI;
+using Sandbox.Game.Gui;
+using Sandbox.Game.World;
+using Sandbox.Graphics;
+using Sandbox.Graphics.GUI;
+using VRage.Game;
 using VRage.Game.Components;
+using VRage.Game.Entity;
+using VRage.Game.Gui;
 using VRage.Game.ModAPI;
+using VRage.Utils;
 using VRageMath;
 
 namespace Rynchodon.Autopilot.Movement
@@ -21,12 +28,36 @@ namespace Rynchodon.Autopilot.Movement
 
 		private const ulong ProfileWait = 200uL;
 
+		private static FieldInfo MyGuiScreenHudSpace_Static;
+		private static MethodInfo MyGuiScreenHudSpace_DrawGravityVectorIndicator;
+
+		private static object[] DrawGravityVectorIndicator_Params = new object[4];
+		private static StringBuilder DrawTitle = new StringBuilder("Air Speed & Drag");
+
+		static AeroEffects()
+		{
+			Type MyGuiScreenHudSpace = typeof(MyGuiScreenHudBase).Assembly.GetType("Sandbox.Game.Gui.MyGuiScreenHudSpace");
+			if (MyGuiScreenHudSpace == null)
+				throw new NullReferenceException("MyGuiScreenHudSpace");
+
+			MyGuiScreenHudSpace_Static = MyGuiScreenHudSpace.GetField("Static", BindingFlags.Static | BindingFlags.Public);
+			if (MyGuiScreenHudSpace_Static == null)
+				throw new NullReferenceException("MyGuiScreenHudSpace_Static");
+
+			MyGuiScreenHudSpace_DrawGravityVectorIndicator = MyGuiScreenHudSpace.GetMethod("DrawGravityVectorIndicator", BindingFlags.Instance | BindingFlags.NonPublic);
+			if (MyGuiScreenHudSpace_DrawGravityVectorIndicator == null)
+				throw new NullReferenceException("MyGuiScreenHudSpace_DrawGravityVectorIndicator");
+
+			DrawGravityVectorIndicator_Params[2] = MyHudTexturesEnum.gravity_arrow;
+		}
+
 		private readonly Logger m_logger;
 		private readonly IMyCubeGrid m_grid;
 
 		private ulong m_profileAt;
 		private float m_airDensity;
 		private bool m_cockpitComplain;
+		private Vector3D m_worldDrag;
 #if DEBUG_DRAW
 		private bool m_profilerDebugDraw;
 		private int m_maxSteps = int.MaxValue;
@@ -76,13 +107,25 @@ namespace Rynchodon.Autopilot.Movement
 
 		public void Update1()
 		{
+			ApplyDrag();
+			Draw();
+		}
+
+		private void ApplyDrag()
+		{
 			if (DragCoefficient == null || m_airDensity == 0f)
+			{
+				m_worldDrag = Vector3D.Zero;
 				return;
+			}
 
 			Vector3 worldVelocity = m_grid.Physics.LinearVelocity;
 
 			if (worldVelocity.LengthSquared() < 1f)
+			{
+				m_worldDrag = Vector3D.Zero;
 				return;
+			}
 
 			MatrixD worldInv = m_grid.WorldMatrixNormalizedInv.GetOrientation();
 			Vector3D localVelocityD; Vector3D.Transform(ref worldVelocity, ref worldInv, out localVelocityD);
@@ -98,15 +141,15 @@ namespace Rynchodon.Autopilot.Movement
 				Vector3 scaled; Vector3.Multiply(ref DragCoefficient[i], Math.Sign(dot) * dot * dot, out scaled);
 				Vector3.Add(ref localDrag, ref scaled, out localDrag);
 
-				m_logger.debugLog("direction: " + direction + ", dot: " + dot + ", scaled: " + scaled);
+				m_logger.traceLog("direction: " + direction + ", dot: " + dot + ", scaled: " + scaled);
 			}
 
 			MatrixD world = m_grid.WorldMatrix.GetOrientation();
-			Vector3D worldDrag; Vector3D.Transform(ref localDrag, ref world, out worldDrag);
+			Vector3D.Transform(ref localDrag, ref world, out m_worldDrag);
 
-			m_logger.debugLog("world velocity: " + worldVelocity + ", local velocity: " + localVelocity + ", local drag: " + localDrag + ", world drag: " + worldDrag);
+			m_logger.traceLog("world velocity: " + worldVelocity + ", local velocity: " + localVelocity + ", local drag: " + localDrag + ", world drag: " + m_worldDrag);
 
-			m_grid.Physics.AddForce(MyPhysicsForceType.APPLY_WORLD_FORCE, worldDrag, null, null);
+			m_grid.Physics.AddForce(MyPhysicsForceType.APPLY_WORLD_FORCE, m_worldDrag, null, null);
 		}
 
 		public void Update100()
@@ -180,6 +223,78 @@ namespace Rynchodon.Autopilot.Movement
 		private void CockpitComplain(Sandbox.Game.Entities.Cube.MyTerminalBlock arg1, StringBuilder arg2)
 		{
 			arg2.AppendLine("ARMS is calculating drag");
+		}
+
+		private void Draw()
+		{
+			MyPlayer player = MySession.Static.LocalHumanPlayer;
+			if (player == null)
+				return;
+
+			MyCubeBlock block = player.Controller.ControlledEntity as MyCubeBlock;
+			if (block == null || block.CubeGrid != m_grid)
+				return;
+
+			if (m_worldDrag == Vector3D.Zero)
+				return;
+
+			MyGuiPaddedTexture backgroundTexture = MyGuiConstants.TEXTURE_HUD_BG_MEDIUM_DEFAULT;
+
+			Vector2 backgroundSize = backgroundTexture.SizeGui + new Vector2(0.017f, 0.05f);
+			backgroundSize.X *= 0.8f;
+			backgroundSize.Y *= 0.75f;
+
+			Vector2 dividerLineSize = new Vector2(backgroundSize.X - backgroundTexture.PaddingSizeGui.X, backgroundSize.Y / 60f);
+
+			Vector2 backgroundPosition = new Vector2(0.01f, backgroundSize.Y + 0.04f);
+			backgroundPosition = ConvertHudToNormalizedGuiPosition(ref backgroundPosition);
+
+			Vector2 titleTextPos = backgroundPosition + backgroundSize * new Vector2(0.94f, -1f) + backgroundTexture.PaddingSizeGui * Vector2.UnitY * 0.2f;
+			Vector2 dividerLinePosition = new Vector2(backgroundPosition.X + backgroundTexture.PaddingSizeGui.X * 0.5f, titleTextPos.Y - 0.022f) + new Vector2(0.0f, 0.026f);
+
+			Vector2 vectorPosition = backgroundPosition - backgroundSize * new Vector2(-0.5f, 0.55f) + backgroundTexture.PaddingSizeGui * Vector2.UnitY * 0.5f;
+			vectorPosition = MyGuiManager.GetHudSize() * ConvertNormalizedGuiToHud(ref vectorPosition);
+
+			MyGuiManager.DrawSpriteBatch(MyGuiConstants.TEXTURE_HUD_BG_MEDIUM_DEFAULT.Texture, backgroundPosition, backgroundSize + new Vector2(0f, 0.025f), Color.White, MyGuiDrawAlignEnum.HORISONTAL_LEFT_AND_VERTICAL_BOTTOM);
+			MyGuiManager.DrawString(MyFontEnum.White, DrawTitle, titleTextPos, MyGuiConstants.HUD_TEXT_SCALE, drawAlign: MyGuiDrawAlignEnum.HORISONTAL_RIGHT_AND_VERTICAL_BOTTOM);
+			MyGuiManager.DrawSpriteBatch(MyGuiConstants.TEXTURE_HUD_GRAVITY_LINE.Texture, dividerLinePosition, dividerLineSize, Color.White, MyGuiDrawAlignEnum.HORISONTAL_LEFT_AND_VERTICAL_CENTER);
+
+			object obj = MyGuiScreenHudSpace_Static.GetValue(null);
+
+			DrawGravityVectorIndicator_Params[0] = vectorPosition;
+			DrawGravityVectorIndicator_Params[1] = Vector3.Normalize(-m_grid.Physics.LinearVelocity);
+			DrawGravityVectorIndicator_Params[3] = Color.GreenYellow;
+			MyGuiScreenHudSpace_DrawGravityVectorIndicator.Invoke(obj, DrawGravityVectorIndicator_Params);
+
+			DrawGravityVectorIndicator_Params[1] = Vector3.Normalize(m_worldDrag);
+			DrawGravityVectorIndicator_Params[3] = Color.Red;
+			MyGuiScreenHudSpace_DrawGravityVectorIndicator.Invoke(obj, DrawGravityVectorIndicator_Params);
+		}
+
+		private static Vector2 ConvertHudToNormalizedGuiPosition(ref Vector2 hudPos)
+		{
+			var safeFullscreenRectangle = MyGuiManager.GetSafeFullscreenRectangle();
+			var safeFullscreenSize = new Vector2(safeFullscreenRectangle.Width, safeFullscreenRectangle.Height);
+			var safeFullscreenOffset = new Vector2(safeFullscreenRectangle.X, safeFullscreenRectangle.Y);
+
+			var safeGuiRectangle = MyGuiManager.GetSafeGuiRectangle();
+			var safeGuiSize = new Vector2(safeGuiRectangle.Width, safeGuiRectangle.Height);
+			var safeGuiOffset = new Vector2(safeGuiRectangle.X, safeGuiRectangle.Y);
+
+			return ((hudPos * safeFullscreenSize + safeFullscreenOffset) - safeGuiOffset) / safeGuiSize;
+		}
+
+		private static Vector2 ConvertNormalizedGuiToHud(ref Vector2 normGuiPos)
+		{
+			var safeFullscreenRectangle = MyGuiManager.GetSafeFullscreenRectangle();
+			var safeFullscreenSize = new Vector2(safeFullscreenRectangle.Width, safeFullscreenRectangle.Height);
+			var safeFullscreenOffset = new Vector2(safeFullscreenRectangle.X, safeFullscreenRectangle.Y);
+
+			var safeGuiRectangle = MyGuiManager.GetSafeGuiRectangle();
+			var safeGuiSize = new Vector2(safeGuiRectangle.Width, safeGuiRectangle.Height);
+			var safeGuiOffset = new Vector2(safeGuiRectangle.X, safeGuiRectangle.Y);
+
+			return ((normGuiPos * safeGuiSize + safeGuiOffset) - safeFullscreenOffset) / safeFullscreenSize;
 		}
 
 #if DEBUG_DRAW
