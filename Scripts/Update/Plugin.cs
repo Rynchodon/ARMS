debe77f464b0743d804cdc90b8387bc01bc48b4a
Rynchodon <al.durand@gmail.com>
2017-01-30 17:03:02 -0500

    DS fix

--- Scripts/Update/Plugin.cs
+++ Scripts/Update/Plugin.cs
@@ -1,6 +1,7 @@
 ﻿using System.Reflection;
 using Sandbox.Common.ObjectBuilders;
 using Sandbox.Definitions;
+using Sandbox.Engine.Platform;
 using Sandbox.Game.World;
 using VRage.ObjectBuilders;
 using VRage.Plugins;
@@ -9,20 +10,30 @@ namespace Rynchodon.Update
 {
 	public class Plugin : IPlugin
 	{
+		private bool _loaded;
+
 		public void Dispose() { }
 
-		public void Init(object gameInstance)
-		{
-			MySession.OnLoading += CheckForArmsAndRegister;
-		}
+		public void Init(object gameInstance) { }
 
-		public void Update() { }
+		public void Update()
+		{
+			bool ready = MySession.Static != null && MySession.Static.Ready;
+
+			if (_loaded != ready)
+			{
+				if (!_loaded)
+					CheckForArmsAndRegister();
+				_loaded = ready;
+			}
+		}
 
 		private static void CheckForArmsAndRegister()
 		{
-			if (MyDefinitionManager.Static.GetCubeBlockDefinition(new SerializableDefinitionId(typeof(MyObjectBuilder_Cockpit), "Autopilot-Block_Large")) == null)
+			if (!Game.IsDedicated && MyDefinitionManager.Static.GetCubeBlockDefinition(new SerializableDefinitionId(typeof(MyObjectBuilder_Cockpit), "Autopilot-Block_Large")) == null)
 				return;
 
+			Logger.DebugLog("Loading ARMS");
 			MySession.Static.RegisterComponentsFromAssembly(Assembly.GetExecutingAssembly(), true);
 		}
 	}
